3fb59f7b28c060c89663d27126373f514046125f
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-04-27 21:46:34 +0200

    Update Contrat.java

--- src/com/simulateur/assurance/Contrat.java
+++ src/com/simulateur/assurance/Contrat.java
@@ -2,7 +2,8 @@ package com.simulateur.assurance;
 
 abstract public class Contrat {
 
-	public abstract void Menu();			
+	public abstract void Menu();
+	public abstract void CalculerTarif();
 	public abstract void Imprimer();		
 	
 }
