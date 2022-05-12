5bf4043a1915cc7ac108d98e125c4bf57c3c9e50
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-05-12 22:25:19 +0200

    Update CalculTarifsContrats.java

--- src/com/simulateur/assurance/CalculTarifsContrats.java
+++ src/com/simulateur/assurance/CalculTarifsContrats.java
@@ -3,7 +3,7 @@ package com.simulateur.assurance;
 public abstract class CalculTarifsContrats extends Contrat{
 	
 	abstract void tarifsAuto();			// Methode pour calculer le tarif auto
-	abstract void tarifsMoto();			// M�thode pour calculer le tarif auto
+	abstract void tarifsMoto();			// Methode pour calculer le tarif auto
 	abstract void tarifsMaison();		       // M�thode pour calculer le tarif auto
 	
 }
