a22d8d20de451516a6d3331dd1609de4c4531dde
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-05-15 23:44:46 +0200

    Update ContratAssuranceVoiture.java

--- src/com/simulateur/assurance/ContratAssuranceVoiture.java
+++ src/com/simulateur/assurance/ContratAssuranceVoiture.java
@@ -4,6 +4,22 @@ public class ContratAssuranceVoiture {
 	
 	public ContratAssuranceVoiture()
 	{
+	
+		private double bonsuMalus;						
+	private int nbAnneeAssurance;						
+	private int nbChevaux;							
+	private double majChevauxTab;					
+	private double nbAnTab;							
+	private double conjointTab;						
+	private double enfantsTab;						
+	private double reductionTab;					
+	int baseAuto = 20;
+	
+
+	double [] majChevauxFiscaux = {1,1.1,1.2,1.3,1.4,1.5,1.6};			
+	int [] anAssur 				= {0,1,2,3,4,5,6};						
+	int [] marieChevaux 		= {1,2,3,4,5,6,7};						
+	int [] enfantMajoration		= {1,2,3,4,5,6,7};	
 		
 	}
 
