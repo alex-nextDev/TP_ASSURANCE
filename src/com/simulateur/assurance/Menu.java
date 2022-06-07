89f9af9a04c45c01146e88c6fdd4c6035e2af5ef
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-06-07 22:37:07 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -60,6 +60,7 @@ public static void saisirInfos() throws InputMismatchException
 			check = true;
 			
 			System.out.print("Veuillez saisir votre date de naissance (dd/mm/yyyy) ? : ");
+			dateNaissance = dateN.next("^([0-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/[0-9]{4}$");
 				
 			  }    
 			  catch (InputMismatchException e)
