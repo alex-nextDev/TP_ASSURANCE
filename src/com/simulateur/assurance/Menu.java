b1135db708b5222a1948c8197e044f2cbb9300d8
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-06-08 23:01:20 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -62,6 +62,7 @@ public static void saisirInfos() throws InputMismatchException
 			System.out.print("Veuillez saisir votre date de naissance (dd/mm/yyyy) ? : ");
 			dateNaissance = dateN.next("^([0-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/[0-9]{4}$");
 			Calendar calendar = Calendar.getInstance();
+			int annee = calendar.get( Calendar.YEAR );	
 				
 			  }    
 			  catch (InputMismatchException e)
