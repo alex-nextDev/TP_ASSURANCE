5db9c2648ce9134f03c62b86eff53185b8b0dcdb
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-07-04 22:16:06 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -193,6 +193,7 @@ public static void saisirInfos() throws InputMismatchException
 						
 						while (conjoint!='o' && conjoint!='O' && conjoint!='n' && conjoint!='N')
 						{
+						System.out.print("Votre conjoint sera assuré ? Oui (O ou o) ou (N ou n)");
 			}
 
 //TO FINISH
