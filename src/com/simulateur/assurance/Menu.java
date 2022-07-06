de7587b32e94cdd63823e2df6bd50222fae34877
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-07-06 21:20:03 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -200,6 +200,7 @@ public static void saisirInfos() throws InputMismatchException
 						  {
 						    System.out.println("Vous devez saisir O ou N");
 						  }
+						  if ()
 			}
 
 //TO FINISH
