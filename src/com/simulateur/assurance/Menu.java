f3c43e886bde1ef694f90b18c61f52d80f3016ec
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-07-22 22:07:32 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -263,8 +263,8 @@ public static void saisirInfos() throws InputMismatchException
 								bonusMalus = Double.parseDouble(chiffreDouble);
 								
 								if (bonusMalus < 0.5 || bonusMalus > 1.5)
-									{								
-										System.out.println("Erreur !!! Vous devez saisir un chiffre entre 0,5 et 1,5\n");
+								{								
+								  System.out.println("Erreur !!! Vous devez saisir un chiffre entre 0,5 et 1,5\n");
 
 //TO FINISH
 }
