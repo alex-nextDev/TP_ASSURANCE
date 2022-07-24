c031c1551e3e698a3d35cf786d692c2119780b37
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-07-24 23:09:52 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -267,6 +267,8 @@ public static void saisirInfos() throws InputMismatchException
 								  System.out.println("Erreur !!! Vous devez saisir un chiffre entre 0,5 et 1,5\n");
 								 }
 								check = true;	
+								}    
+							  catch (InputMismatchException e)
 
 //TO FINISH
 }
