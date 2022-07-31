9bc812f504a114aade4a14e231f7ad18547370d7
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-07-31 21:38:21 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -298,7 +298,7 @@ public static void saisirInfos() throws InputMismatchException
 							}
 						
 						}
-						Client clientsMoto = new Client();
+						Client clientsMoto = new Client(nom);
 
 //TO FINISH
 }
