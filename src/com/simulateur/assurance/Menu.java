cadc8a222f05ee7bb01799a02fd0527a556d3a63
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-08-01 23:17:12 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -300,7 +300,7 @@ public static void saisirInfos() throws InputMismatchException
 						}
 						Client clientsMoto = new Client(nom, prenom, dateNaissance, statut, nbEnfants);
 						
-						ContratAssuranceMoto contratmoto = new ContratAssuranceMoto(cylindree) ;
+						ContratAssuranceMoto contratmoto = new ContratAssuranceMoto(cylindree, anneeAssurance) ;
 
 //TO FINISH
 }
