e74d21985fc1f1426179e62c2b230baf893c2b40
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-08-17 23:07:22 +0200

    Update Menu.java

--- src/com/simulateur/assurance/Menu.java
+++ src/com/simulateur/assurance/Menu.java
@@ -330,7 +330,7 @@ public static void saisirInfos() throws InputMismatchException
 							  check = false;
 							}
 							
-							}
+						}
 						while (check == false || surface < 10);
 						
 						break;
